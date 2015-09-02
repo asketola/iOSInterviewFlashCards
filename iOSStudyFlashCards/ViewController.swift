2ebf05b640c47d6ddb90a3bb29965a7676796d5e
Anne <star7anne@yahoo.com>
2015-09-02 12:33:57 -0700

    removed space

--- iOSStudyFlashCards/ViewController.swift
+++ iOSStudyFlashCards/ViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class ViewController: UIViewController {
   
   @IBOutlet weak var headerLabel: UILabel!
-  
   @IBOutlet weak var pickTopicLabel: UILabel!
   
   var typeOfQuestion = String()
